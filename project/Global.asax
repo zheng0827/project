a348ca1fc98199425bc90a9416e694c59e02f730
zheng0827 <ray950827@gmail.com>
2025-11-19 00:17:00 +0800

    加入專案檔案。

---
+++ project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="project.Global_asax" Language="vb" %>
---
+++ project/My Project/AssemblyInfo.vb
@@ -0,0 +1,31 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' 組件的一般資訊是由下列的屬性集 
+' 控制。變更這些屬性值可修改與組件關聯的
+' 資訊。
+
+' 檢閱組件屬性的值
+<Assembly: AssemblyTitle("project")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("project")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'下列 GUID 為專案公開 (Expose) 至 COM 時所要使用的 typelib ID
+<Assembly: Guid("2a571f8d-b885-4f34-aa49-09119685b146")>
+
+' 組件的版本資訊是由下列四項值構成:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
